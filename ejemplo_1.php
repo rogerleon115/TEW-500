279573434944a0dfb5e27655c0a65eebff63059a
rogerleon115 <65256979+rogerleon115@users.noreply.github.com>
2020-05-12 19:23:24 -0400

    Create ejemplo_1.php

---
+++ ejemplo_1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> mi primera pagina web </h1>
+    <?php
+    echo "bienvenido a tew-500"
+    ?>
+</body>
+</html>
